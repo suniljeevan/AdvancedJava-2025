62488b299e53df86ccbd6c4605789ceb92516360
suniljeevan <36226091+suniljeevan@users.noreply.github.com>
2025-01-29 10:24:34 +0530

    Abstraction
    TEST3

---
+++ COREJAVAREVISION/src/MYPACK1/TEST3.java
@@ -0,0 +1,42 @@
+package MYPACK1;
+// Abstract Relationship
+// Loose-coupling between parent and child
+// Some implementation of parents are hidden
+abstract class Shape { 
+	// zero or methods are abstract
+	public abstract void area();
+	public abstract void volume();
+	public void showRules() {
+		System.out.println(" Area is for 2D shape");
+		System.out.println("Volume is for 3D shape");
+	}
+}
+class ShapeImpl extends Shape {
+	int dimension;
+    //alt shift shift
+	public ShapeImpl(int dimension) {
+		super();
+		this.dimension = dimension;
+	}
+	public void area() { 
+		if(dimension==2) 
+			System.out.println("area is "+3.141*5*5);
+		else 
+			System.out.println("Not possible");
+	}
+	public void volume() {
+		if(dimension==3) 
+			System.out.println("Volume is "+(5*5*5));
+		else 
+			System.out.println("Not possible");
+	}
+	
+}
+public class TEST3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
