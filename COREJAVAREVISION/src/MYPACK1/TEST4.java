ac18c202e87bb2eb50b895e90913294b1b48beb6
suniljeevan <36226091+suniljeevan@users.noreply.github.com>
2025-01-30 13:01:58 +0530

    Interface
    Test4

---
+++ COREJAVAREVISION/src/MYPACK1/TEST4.java
@@ -0,0 +1,27 @@
+package MYPACK1;
+//pure abstraction
+// all methods are abstract 
+// Interface makes all methods abstract
+interface Shappe {
+	// public abstract is not necessary
+	 void area();
+	 public void volume();
+	 // non abstract method is not allowed
+}
+class Circle implements Shappe {
+	private double radious;
+	public void area() {
+		
+		
+	}
+	 public void volume() {}
+	
+	
+}
+public class TEST4 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
