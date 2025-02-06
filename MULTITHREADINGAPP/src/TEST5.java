8d3fceaf760bd1f302c9107b6d90d13fcef4a35c
suniljeevan <36226091+suniljeevan@users.noreply.github.com>
2025-02-06 13:57:20 +0530

    TEST5
    parallel or concurrent execution of 2 threads using sleep()

--- MULTITHREADINGAPP/src/TEST5.java
+++ MULTITHREADINGAPP/src/TEST5.java
@@ -7,18 +7,23 @@ import java.util.Scanner;
 
 class Task_primecheck implements Runnable {
 	public void run() {
+		try {
 		int n,c=0;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a number");
 		n=sc.nextInt();
 		for(int i=1;i<=n/2;i++) {
-			if(n%i==0)
+			System.out.println(Thread.currentThread()+" "+i);
+			Thread.sleep(3000);
+			if(n%i==0) {
 				c++;
+			}
 		}
 		if(c==1)
 			System.out.println(n+" is prime");
 		else
 			System.out.println(n+" is not prime");
+	}catch(Exception e) {}
 	}
 }
 public class TEST5 {
@@ -26,12 +31,8 @@ public class TEST5 {
 		Task_primecheck task=new Task_primecheck();
          Thread t1=new Thread(task,"first");
          Thread t2=new Thread(task,"second");
-         Thread t3=new Thread(task,"third");
-         Thread t4=new Thread(task,"fourth");
-         Thread t5=new Thread(task,"fifth");
          t1.start();
-         t2.start();t3.start();
-         t4.start();t5.start();
+         t2.start();
          System.out.println("main thread ended");
 	}
 
