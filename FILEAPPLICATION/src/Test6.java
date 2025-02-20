fa40163809092ea4beb1ca4b426a7277a5e787ab
suniljeevan <36226091+suniljeevan@users.noreply.github.com>
2025-02-20 13:32:08 +0530

    Test6
    done

---
+++ FILEAPPLICATION/src/Test6.java
@@ -0,0 +1,36 @@
+/*
+ Write a Java program to perform the following 
+ operations with text files.
+a)	Display the number of characters, 
+ words and sentences present in a text file 
+multithread.txt.
+b)	Display the content of the file on the screen 
+with line number before each line.
+ */
+import java.io.*;
+public class Test6 {
+	public static void main(String[] args) throws Exception{
+		File file=new File("C:\\Presidency\\multithread.txt");
+		FileReader fr=new FileReader(file);
+		char ch;
+		int cc=0,i;
+		while((i=fr.read())!=-1) {
+			ch=(char)i;
+			cc++;
+		}
+		fr.close();
+		int cw=0;
+		FileReader fr2=new FileReader(file);
+		BufferedReader br=new BufferedReader(fr2);
+		String line;
+		while((line=br.readLine())!=null) {
+			String[] words=line.split(" ");
+			cw=cw+words.length;
+		}
+		System.out.println("No. of characters is "+cc);
+		System.out.println("No. of words is "+cw);
+		fr2.close();
+
+	}
+
+}
